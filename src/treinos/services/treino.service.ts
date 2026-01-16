09abd1487eb6e378698b1f1ab9251ef933b4fb82
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-01-16 11:07:12 -0300

    atualização treino

--- src/treinos/services/treino.service.ts
+++ src/treinos/services/treino.service.ts
@@ -3,35 +3,35 @@ import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class TreinoService {
-  constructor(private temaRepository: Repository<Tema>) {}
+  constructor(private treinoRepository: Repository<Treino>) {}
 
-  async findAll(): Promise<Tema[]> {
+  async findAll(): Promise<Treino[]> {
     return await this.temaRepository.find();
   }
 
-  async findById(id: number): Promise<Tema> {
-    const tema = await this.temaRepository.findOne({
+  async findById(id: number): Promise<Treino> {
+    const treino = await this.temaRepository.findOne({
       where: { id },
     });
 
-    if (!tema) {
+    if (!treino) {
       throw new HttpException('Tema not found', HttpStatus.NOT_FOUND);
     }
 
-    return tema;
+    return treino;
   }
 
-  async create(tema: Tema): Promise<Tema> {
-    return await this.temaRepository.save(tema);
+  async create(treino: treino): Promise<Treino> {
+    return await this.treinoRepository.save(treino);
   }
 
-  async update(tema: Tema): Promise<Tema> {
-    const tema = await this.findById(tema.id);
-    return await this.temaRepository.save(tema);
+  async update(treino: treino): Promise<Treino> {
+    const treino = await this.findById(treino.id);
+    return await this.treinoRepository.save(treino);
   }
 
   async delete(id: number): Promise<DeleteResult> {
     await this.findById(id);
-    return await this.temaRepository.delete(id);
+    return await this.treinoRepository.delete(id);
   }
 }
